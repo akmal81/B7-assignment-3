79114d0bc32328ff62de4812b332a3b6fd9ac8bd
akmal81 <sah64078@gmail.com>
2026-06-14 00:15:48 +0600

    databas and shcema created and data inserted

---
+++ query.sql
@@ -0,0 +1,57 @@
+
+DROP TABLE IF EXISTS Bookings;
+DROP TABLE IF EXISTS Matches;
+DROP TABLE IF EXISTS Users;
+
+CREATE TABLE Users (
+    user_id INT PRIMARY KEY,
+    full_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE,
+    role VARCHAR(20) CHECK(role IN ('Ticket Manager', 'Football Fan')),
+    phone_number VARCHAR(20)  
+);
+ 
+CREATE TABLE Matches (
+    match_id INT PRIMARY KEY,
+    fixture VARCHAR(100) NOT NULL,
+    tournament_category VARCHAR(50) NOT NULL,
+    base_ticket_price DECIMAL(10, 2) ,
+    match_status VARCHAR(20)
+
+    CONSTRAINT CHK_MATCHES_BASE_TICKET_PRICE CHECK(base_ticket_price > 0),
+    CONSTRAINT CHK_MATCHES_MATCH_STATUS CHECK(match_status IN ('Available', 'Selling Fast', 'Sold Out', 'Postponed'))
+  );
+  
+CREATE TABLE Bookings (
+    booking_id INT PRIMARY KEY,
+    user_id INT REFERENCES users(user_id),
+    match_id INT REFERENCES matches(match_id),
+    seat_number VARCHAR(20),
+    payment_status VARCHAR(20),
+    total_cost DECIMAL(10,2) 
+
+    CONSTRAINT CHK_BOOKING_PAYMENT_STATUS CHECK(payment_status IN('Pending', 'Confirmed', 'Cancelled', 'Refunded')),
+    CONSTRAINT CHK_BOOKINGS_TOTAL_COST CHECK(total_cost > 0)
+);
+
+
+INSERT INTO Users (user_id, full_name, email, role, phone_number) VALUES
+(1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
+(2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
+(3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
+(4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
+
+
+INSERT INTO Matches (match_id, fixture, tournament_category, base_ticket_price, match_status) VALUES
+(101, 'Real Madrid vs Barcelona', 'Champions League', 150.00, 'Available'),
+(102, 'Man City vs Liverpool', 'Premier League', 120.00, 'Selling Fast'),
+(103, 'Bayern Munich vs PSG', 'Champions League', 130.00, 'Available'),
+(104, 'AC Milan vs Inter Milan', 'Serie A', 90.00, 'Sold Out'),
+(105, 'Juventus vs Roma', 'Serie A', 80.00, 'Available');
+
+INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status, total_cost) VALUES
+(501, 1, 101, 'A-12', 'Confirmed', 150.00),
+(502, 1, 102, 'B-04', 'Confirmed', 120.00),
+(503, 2, 101, 'A-13', 'Confirmed', 150.00),
+(504, 2, 101, NULL, NULL, 150.00),
+(505, 3, 102, 'C-20', 'Pending', 120.00);
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Assignment 3
+ERD diagram Link : https://lucid.app/lucidchart/7354ab36-25b1-419a-8460-6a552f10930c/edit?viewport_loc=79%2C217%2C3667%2C2208%2C0_0&invitationId=inv_99a17b90-4cba-4196-b78c-f98679f86db2
